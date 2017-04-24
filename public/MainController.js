9df585b4188f409e0055a897f119089f423b36be
junedev <catherinejune86@gmail.com>
2017-04-24 19:39:19 +0200

    add images and questions

--- public/MainController.js
+++ public/MainController.js
@@ -8,13 +8,28 @@ function MainController($scope, $routeParams, $window, $firebaseObject) {
     var ref = firebase.database().ref().child("data");
     var syncObject = $firebaseObject(ref);
     syncObject.$bindTo($scope, "data");
-    var image = "http://geniussys.com/img/placeholder/blogpost-placeholder-100x100.png"
     self.teams = [
-        {id: 1, name: 'Team 1', image},
-        {id: 2, name: 'Team 2', image},
-        {id: 3, name: 'Team 3', image},
-        {id: 4, name: 'Team 4', image},
-        {id: 5, name: 'Team 5', image}
+        {id: 'security', name: 'IT-Security', image: './images/security.jpg'},
+        {id: 'pm', name: 'IT-Projektmanagement', image: './images/pm.jpg'},
+        {id: 'iot', name: 'Internet of Things', image: './images/iot.jpg'},
+        {id: 'games', name: 'Spiele-Entwicklung', image: './images/games.jpg'},
+        {id: 'backend', name: 'Backend-Entwicklung', image: './images/backend.jpg'},
+        {id: 'beratung', name: 'IT-Beratung', image: './images/beratung.jpg'}
+    ]
+
+    self.questions = [
+       'Wie gelangen Internetseiten in den Browser?',
+       'Warum wird Ruby als einfache Programmiersprache bezeichnet?',
+       'Wofür benötigen Unternehmen IT–Projektmanager?',
+       'Was unterscheidet einen IT-Projektmanager von einem normalen Projektmanager?',
+       'Wie nennt man jemanden, der sich unberechtigten Zugriff auf einen fremden Computer verschafft?',
+       'Wie sieht ein sicheres Passwort aus?',
+       'Auf welchen vier Gewerken beruht die Game Entwicklung?',
+       'Was ist der Unterschied zwischen Digital Art und Game Design?',
+       'Was bedeutet das Logo am Stand IT Beratung?',
+       'Welche Fähigkeiten brauchen IT Berater/Innen?',
+       'Wer hat den Begriff "Internet of Things" erstmals verwendet und wann war das ungefähr?',
+       'Welchen Zusammenhang gibt es zwischen dem "Internet der Dinge" und Mode?'
     ]
 
     self.routeParams = $routeParams
